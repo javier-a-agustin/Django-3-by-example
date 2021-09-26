035415d641399284ca5c3e563dbcfb30207f1fad
Javier Fernandez <22.javier.fernandez@gmail.com>
2021-09-25 21:10:34 -0300

    Add Chapter 1 tests

--- Chapter 1/mysite/blog/tests.py
+++ Chapter 1/mysite/blog/tests.py
@@ -1,3 +1,93 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
+from django.test import Client
+from django.contrib.auth.models import User, AnonymousUser
+from django.utils import timezone
 
-# Create your tests here.
+from .models import Post
+
+from .views import PostListView
+
+class PostListTest(TestCase):
+	def setUp(self):
+		self.client = Client()
+		self.author = User.objects.create_user('testuser', 'test@user.com', 'secretpassword')
+		post = Post.objects.create(
+			title	= 'Test Post'
+			, slug	= 'test-post'
+			, author = self.author
+			, status = 'published'
+		)
+	def test_post_list(self):
+		"""
+			 
+		"""
+		response = self.client.get('/blog/')
+		# Check that the response is 200 OK.
+		self.assertEqual(response.status_code, 200)
+		# Check that the rendered context contains 1 post.
+		self.assertEqual(len(response.context['posts']), 1)
+
+	def test_post_list_context(self):
+		"""
+		
+		"""
+		Post.objects.create(
+			title	= 'Test Post 1'
+			, slug	= 'test-post-1'
+			, author = self.author
+			, status = 'published'
+		)
+		Post.objects.create(
+			title	= 'Test Post 2'
+			, slug	= 'test-post-2'
+			, author = self.author
+			, status = 'published'
+		)
+		Post.objects.create(
+			title	= 'Test Post 3'
+			, slug	= 'test-post-3'
+			, author = self.author
+			, status = 'draft'
+		)
+		response = self.client.get('/blog/')
+		# Check that the response is 200 OK.
+		self.assertEqual(response.status_code, 200)
+		# Check that the rendered context contains 1 post.
+		self.assertEqual(len(response.context['posts']), 3)
+	
+	def test_post_detail(self):
+		"""
+		
+		"""
+		today = timezone.now()
+		today = today.strftime("%Y/%m/%d")
+
+		response = self.client.get(f'/blog/{today}/test-post/')
+		self.assertEqual(response.status_code, 200)
+	
+	def test_post_detail_context(self):
+		Post.objects.create(
+			title	= 'Test Post 2'
+			, slug	= 'test-post-2'
+			, author = self.author
+			, status = 'published'
+		)
+
+		Post.objects.create(
+			title	= 'Test Post 3'
+			, slug	= 'test-post-3'
+			, author = self.author
+			, status = 'draft'
+		)
+		today = timezone.now()
+		today = today.strftime("%Y/%m/%d")
+
+		response = self.client.get(f'/blog/{today}/test-post/')
+		self.assertEqual(response.status_code, 200)
+
+		response = self.client.get(f'/blog/{today}/test-post-2/')
+		self.assertEqual(response.status_code, 200)
+
+		response = self.client.get(f'/blog/{today}/test-post-3/')
+		self.assertEqual(response.status_code, 404)
+
